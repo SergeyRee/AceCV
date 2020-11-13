dc79a48ad1cdcbdbca91897d4a7138b55d04a7ff
SergeyRee <drrons2000@gmail.com>
2020-11-13 19:31:30 +0100

    first

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* Responsive Menu */
+const burgerIcon = document.querySelector('#menu-toggle');
+const menu = document.querySelector('#mobile-nav');
+
+burgerIcon.onclick = function() {
+    menu.classList.toggle('mobile-nav-active')
+}
