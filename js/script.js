ed8946c233bd670b48de7777da3d3657539c3229
SergeyRee <drrons2000@gmail.com>
2020-11-14 00:02:36 +0100

    1111

--- js/script.js
+++ js/script.js
@@ -4,4 +4,10 @@ const menu = document.querySelector('#mobile-nav');
 
 burgerIcon.onclick = function() {
     menu.classList.toggle('mobile-nav-active')
-}
+};
+
+const navItem = document.querySelectorAll('.nav__item');
+
+navItem.onclick = navItem.classList.add('nav__item-active');
+
+console.log(navItem);
