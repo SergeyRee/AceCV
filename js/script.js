1f36fdcf752f62db1b528aeba6ff5d0f11989179
SergeyRee <drrons2000@gmail.com>
2020-11-22 17:34:40 +0100

    header fixed

--- js/script.js
+++ js/script.js
@@ -5,3 +5,23 @@ const menu = document.querySelector('#mobile-nav');
 burgerIcon.onclick = function() {
     menu.classList.toggle('mobile-nav-active')
 };
+
+/* Active MEnu Items*/
+
+window.addEventListener('scroll', () => {
+    let scrollDistance = window.scrollY;
+
+    console.log(scrollDistance);
+
+    document.querySelectorAll('section').forEach((el, i) => {
+        if(el.offsetTop - document.querySelector('.header').clientHeight <= scrollDistance) {
+            document.querySelectorAll('nav a').forEach((el) => {
+                if (el.classList.contains('nav-active')) {
+                    el.classList.remove('active');
+                }
+            });
+
+            document.querySelectorAll('nav li')[i].querySelector('a').classList.add('active')
+        }
+    });
+});
