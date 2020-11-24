c1149bbae16bb1e807731f18e201f951a1d0709f
SergeyRee <drrons2000@gmail.com>
2020-11-24 16:23:27 +0100

    html final v1.0

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 /* Responsive Menu */
 const burgerIcon = document.querySelector('#menu-toggle');
 const menu = document.querySelector('#mobile-nav');
+const body = document.querySelector('body');
 
-burgerIcon.onclick = function() {
-    menu.classList.toggle('mobile-nav-active')
+burgerIcon.onclick = function () {
+    menu.classList.toggle('mobile-nav-active');
+    body.classList.toggle('modal-open');
 };
+
 /* /Responsive Menu */
+
+const header = document.querySelector(".header");
+
+function checkScroll() {
+    let scrollPos = window.scrollY;
+
+    if (scrollPos > 300) {
+        header.classList.add("scroll");
+        header.classList.remove("no-scroll");
+    } else {
+        header.classList.add("no-scroll");
+        header.classList.remove("scroll");
+    }
+}
+
+window.addEventListener("scroll", checkScroll);
+document.addEventListener("DOMContentLoaded", checkScroll);
+
 
 /* Active MEnu Items*/
 window.addEventListener('scroll', () => {
@@ -14,7 +35,7 @@ window.addEventListener('scroll', () => {
     console.log(scrollDistance);
 
     document.querySelectorAll('section').forEach((el, i) => {
-        if(el.offsetTop - document.querySelector('.header').clientHeight <= scrollDistance) {
+        if (el.offsetTop - document.querySelector('.header').clientHeight <= scrollDistance) {
             document.querySelectorAll('nav a').forEach((el) => {
                 if (el.classList.contains('nav__link-active')) {
                     el.classList.remove('nav__link-active');
